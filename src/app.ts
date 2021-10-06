8031b5e4dda9808205aeb18981c414695ac1c14a
Wesley Araujo <wesleyfaraujjo@gmail.com>
2021-10-05 22:26:49 -0300

    🔧 Update configurations files

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,7 @@
 import express from 'express';
 import routes from './routes/index';
-import "reflect-metadata";
-import { createConnection } from 'typeorm';
 
 const app = express();
-createConnection()
 
 app.use(express.json());
 app.use(routes);
