82e86ed1c1a08a92434ea30f7cedb555073dd4c9
RegioNT <cleoreggio@gmail.com>
2021-11-09 20:49:32 -0300

    :sparkles: add migration

---
+++ src/database/migration/1636498950124-create_relation.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createRelation1636498950124 implements MigrationInterface {
+    name = 'createRelation1636498950124'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "public"."client" ADD "birthday" TIMESTAMP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "public"."product" ALTER COLUMN "lovers" DROP NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "public"."product" ALTER COLUMN "lovers" SET NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "public"."client" DROP COLUMN "birthday"`);
+    }
+
+}
